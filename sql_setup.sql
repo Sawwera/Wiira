6dc728316330aa7c74d53ef04b82d729fe9073c2
Sawwera <56625359+Sawwera@users.noreply.github.com>
2020-09-08 20:37:54 +0800

    Create sql_setup.sql

---
+++ sql_setup.sql
@@ -0,0 +1,9 @@
+CREATE TABLE user(
+  fullname VARCHAR (255) NOT NULL,
+  phonenumber INTEGER (50) NOT NULL,
+  address VARCHAR (255) NOT NULL,
+  gName VARCHAR (255) NOT NULL,
+  gNumber INTEGER (50) NOT NULL,
+  email VARCHAR (255) NOT NULL,
+  password INTEGER (50) NOT NULL
+);
